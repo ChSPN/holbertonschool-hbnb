16af171ec00a589dfa6d54580b8ebe2c01a04d4a
ChSPN <charlene.scomparin@gmail.com>
2024-06-09 16:26:45 +0200

    init project commit

---
+++ reviewRepository.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+import uuid
+from review import Review
+
+class ReviewRepository(ABC):
+    @abstractmethod
+    def create(self, review:Review) -> bool: pass
+
+    @abstractmethod
+    def get_by_place(self, id:uuid) -> list[Review]: pass
