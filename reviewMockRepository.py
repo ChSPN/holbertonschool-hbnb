096ec2e6ce7d2dd2ad2ff144cc47042e27c1e679
ChSPN <charlene.scomparin@gmail.com>
2024-06-09 16:27:15 +0200

    add the unit tests

---
+++ reviewMockRepository.py
@@ -0,0 +1,15 @@
+import uuid
+from reviewRepository import ReviewRepository
+from review import Review
+
+class ReviewMockRepository(ReviewRepository):
+    def __init__(self, create = bool or None, get_by_place = [] or None):
+        super().__init__()
+        self._create = create
+        self._get_by_place = get_by_place
+
+    def create(self, review:Review) -> bool:
+        return self._create
+
+    def get_by_place(self, id:uuid) -> list[Review]:
+        return self._get_by_place
