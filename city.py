80acc6a16bcd8c4166813bca71e8afd9896b2e78
ChSPN <charlene.scomparin@gmail.com>
2024-06-09 16:26:45 +0200

    init project commit

---
+++ city.py
@@ -0,0 +1,10 @@
+import uuid
+
+class City:
+    def __init__(self):
+        self.country = None
+        self.country_id = uuid
+        """Foreign key of country"""
+        self.icon = str or None
+        self.id = uuid.uuid4()
+        self.name = str
